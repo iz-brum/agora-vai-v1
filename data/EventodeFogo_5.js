b50cca8b34f5c6c2355dba8bc5adaa188d5f273b
iz-Brum <izann.brum@alunosenai.mt>
2024-10-30 17:03:49 -0400

    Adicionando arquivos grandes ao Git LFS

---
+++ data/EventodeFogo_5.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ceaa13aabea37c5b311ab00c0e1287905ae8452a253eddc3839326dabf5ee139
+size 57161316
---
+++ data/EventodeFogo_7.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d5718c6eefe584f191a9e01c7034befacf39fa9ed5c92a0f5374025a44321d24
+size 57161316
